9425c429fcb6a8c6a354445824ade011f59b2225
Silvio Le Pera <s.lepera@software-inside.it>
2024-02-12 10:39:17 +0100

    Aggiunti username e password ai microservizi per l'autenticazione al discovery server

--- discovery-server/src/main/java/com/silvio/discoveryservice/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/silvio/discoveryservice/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,7 +37,7 @@ public class SecurityConfig {
                         auth
                                 .anyRequest() // Ogni richiesta fatta a questo servizio
                                 .hasAuthority("USER") // deve avere come autorità "USER"
-                ).httpBasic();
+                ).httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
